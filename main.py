f40ad474ceaafef4d016b867655f93c5af84f376
KnightAlice <53329069+KnightAlice@users.noreply.github.com>
2023-11-03 21:47:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import apis
 import logs
 import datetime
-
+import time
 
 SERVICE_CATEGORY = "2c9c486e4f821a19014f82381feb0001"  # This is the category ID for "Sports Reservation". It usually doesn't change.
 
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     print("运行时间：", formatted_time)
     while(formatted_time!="230000"):
         now = datetime.datetime.now()
-        formatted_time = now.strftime("%H:%M:%S")
-        time.sleep(1)
+        formatted_time = now.strftime("%H%M%S")
+        time.sleep(0.3)
     try:
         logged_in_session = apis.login(USER_ID, USER_PASSWORD)
         campus_id, sport_id = apis.load_sports_and_campus_id(logged_in_session, SERVICE_CATEGORY, CAMPUS_NAME, SPORT_NAME)
