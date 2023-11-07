89d0169cef9a16fe2333759d7bfd5701db6dede9
KnightAlice <53329069+KnightAlice@users.noreply.github.com>
2023-11-07 09:42:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ USER_PASSWORD = "Mmmmjl123123"
 CAMPUS_NAME = "邯郸校区"
 SPORT_NAME = "网球"
 SPORT_LOCATION = "南区网球场"
-DATE = "2023-11-09"
+DATE = "2023-11-10"
 TIME_1 = "20:00"
 TIME_2 = "21:00"
 TIME_3 = "19:00"
@@ -27,15 +27,17 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
     formatted_time = now.strftime("%M%S")
     print("运行时间：", formatted_time)
-    while(formatted_time!="0000"):
+    while(formatted_time!="0001"):
         now = datetime.datetime.now()
         formatted_time = now.strftime("%M%S")
         time.sleep(0.3)
+    print("到达时间：", formatted_time)
     try:
         logged_in_session = apis.login(USER_ID, USER_PASSWORD)
         campus_id, sport_id = apis.load_sports_and_campus_id(logged_in_session, SERVICE_CATEGORY, CAMPUS_NAME, SPORT_NAME)
         service_id = apis.get_service_id(logged_in_session, SERVICE_CATEGORY, campus_id, sport_id, SPORT_LOCATION)
-        
+        formatted_time = now.strftime("%M%S")
+        print("开始时间：", formatted_time)
         apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_1)
         apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_2)
         apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_3)
