cf8cdc4a0bb50916ee2623dc7ed0e8305f0e36c5
KnightAlice <53329069+KnightAlice@users.noreply.github.com>
2023-11-08 22:12:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,9 +39,21 @@ if __name__ == '__main__':
         formatted_time = now.strftime("%M%S")
         print("开始时间：", formatted_time)
         apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_1)
-        apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_2)
-        apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_3)
-        apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_4)
+        #apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_2)
+        #apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_3)
+        #apis.reserve(logged_in_session, service_id, SERVICE_CATEGORY, DATE, TIME_4)
+        if EMAILS:
+                import smtplib
+                import datetime
+                message = f"Reserve Finished."
+                connection = smtplib.SMTP("smtp-mail.outlook.com", 587)
+                try:
+                    connection.ehlo()
+                    connection.starttls()
+                    connection.login(YOUR_EMAIL, EMAIL_PASSWORD)
+                    connection.sendmail(YOUR_EMAIL, EMAILS, message)
+                finally:
+                    connection.quit()
     except Exception as e:
         if EMAILS:
             import smtplib
